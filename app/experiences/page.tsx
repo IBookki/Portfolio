a3621a3ab6a20fbeabfb93e65c8ada17d375f4cf
IBookki <jubloutheo0@gmail.com>
2025-06-08 12:25:22 +0200

    add experiences

--- app/experiences/page.tsx
+++ app/experiences/page.tsx
@@ -12,18 +12,38 @@ const ExperiencesPage = () => {
     <main className="min-h-screen p-8">
       <h1 className="text-3xl font-extrabold mb-6">My Experiences</h1>
       <div className="grid gap-6">
+
         <div className="p-6 border rounded shadow-md">
-          <h2 className="text-slate-100 text-xl font-bold">Cashier</h2>
-          <p className="text-gray-600">
-            Carrefour • August 2024 -{" "}
-            <span className="text-amber-400">Present</span>
-          </p>
+          <h2 className="text-slate-100 text-xl font-bold">Stage</h2>
+          <p className="text-gray-600">Krys Vincennes • June - July 2025</p>
           <p className="text-slate-100 mt-2 font-medium">
-            Take charge of the customer, collect the products, guide the
-            customer through the different methods of collection.
+            Two-month internship to design a website for the Krys store
           </p>
           <p className="text-slate-300 mt-2">
-            Skills acquired: Customer service, Teamwork, Thoroughness
+            Structured with NextJS, Front made using Tailwind and Javascript
+          </p>
+          <p className="text-slate-300 mt-2">
+            The store can add new eyewear models itself.
+          </p>
+        </div>
+
+        <div className="p-6 border rounded shadow-md">
+          <h2 className="text-slate-100 text-xl font-bold">Expert code</h2>
+          <p className="text-gray-600">
+            IIM Project Grant • February - March 2025
+          </p>
+          <p className="text-slate-100 mt-2 font-medium">
+             Customer : Ville du Plessis-Robinson-Year 2 final validation project: Design of a forum and a showcase site
+              for the Maison des Partages de la Ville du Plessis-Robinson.
+          </p>
+          <p className="text-slate-300 mt-2">
+            Technologies used: NextJS (React/HTML/CSS), Tailwind, Typescript (Javascript)
+          </p>
+          <p className="text-slate-300 mt-2">
+            Database: noSQL with MongoDB
+          </p>
+          <p className="text-slate-300 mt-2">
+            Post system in CRUD, replies, like, popularity and post search
           </p>
         </div>
 
@@ -48,6 +68,21 @@ const ExperiencesPage = () => {
           <p className="text-slate-300 mt-2">
             Implementation of communication strategies to make the museum more
             attractive
+          </p>
+        </div>
+
+                <div className="p-6 border rounded shadow-md">
+          <h2 className="text-slate-100 text-xl font-bold">Cashier</h2>
+          <p className="text-gray-600">
+            Carrefour • August 2024 -{" "}
+            <span className="text-amber-400">Present</span>
+          </p>
+          <p className="text-slate-100 mt-2 font-medium">
+            Take charge of the customer, collect the products, guide the
+            customer through the different methods of payment.
+          </p>
+          <p className="text-slate-300 mt-2">
+            Skills acquired: Customer service, Teamwork, Thoroughness
           </p>
         </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -10,14 +10,14 @@ export const metadata: Metadata = {
 export default function AboutMe() {
   return (
     <div className="self-center min-h-[calc(100vh-60px)] flex-col items-center justify-center p-10">
-      <p className="pt-12 text-center text-lg text-slate-200 opacity-90">
+      <p className="pt-12 text-justify text-lg text-slate-200 opacity-90">
         My name is Theo Jublou, and I am currently studying at IIM Digital
         School, where I specialize in digital technologies and programming.
         During my studies, I have learned various programming languages and
         frameworks, including Next.js, PHP, and TypeScript.
       </p>
 
-      <p className="pt-4 text-center text-lg text-slate-200 opacity-90">
+      <p className="pt-4 text-justify text-lg text-slate-200 opacity-90">
         My goal is to become a skilled full-stack developer or a digital project
         manager—roles in which I can combine technical expertise with project
         coordination. My passion for digital technologies was sparked in high
@@ -25,35 +25,35 @@ export default function AboutMe() {
         to take Python coding classes.
       </p>
 
-      <p className="pt-4 text-center text-lg text-slate-200 opacity-90">
+      <p className="pt-4 text-justify text-lg text-slate-200 opacity-90">
         This experience opened my eyes to the endless possibilities of
         technology and confirmed my desire to pursue a career in this dynamic
         and constantly evolving field.
       </p>
 
-      <p className="pt-8 text-center text-lg text-slate-200 opacity-90">
+      <p className="pt-8 text-justify text-lg text-slate-200 opacity-90">
         In addition to my academic background, I have developed strong
         professional skills through various work experiences. Working at
         McDonald&apos;s taught me teamwork, adaptability, and the importance of
         organization in a fast-paced environment.
       </p>
 
-      <p className="pt-4 text-center text-lg text-slate-200 opacity-90">
+      <p className="pt-4 text-justify text-lg text-slate-200 opacity-90">
         My role at the town hall helped me develop administrative skills and
-        attention to detail, while my current position as a cashier has enhanced
-        my customer service abilities, including resolving conflicts calmly and
+        attention to detail, while my current position as a cashier at Carrefour has enhanced
+        my customer service abilities, including resolving conflicts peacefully and
         professionally. These experiences have strengthened my communication
         skills and my ability to work effectively under pressure.
       </p>
 
-      <p className="pt-8 text-center text-lg text-slate-200 opacity-90">
+      <p className="pt-8 text-justify text-lg text-slate-200 opacity-90">
         I am eager to join a dynamic and supportive team where I can grow both
         professionally and personally. I thrive in environments with clear
         opportunities for evolution and improvement, as they motivate me to give
         my best every day.
       </p>
 
-      <p className="pt-8 text-center text-lg text-slate-200 opacity-90">
+      <p className="pt-8 text-justify text-lg text-slate-200 opacity-90">
         Outside of work, I love playing video games and I also stay active
         through gym workouts and calisthenics, and I enjoy meeting new people to
         broaden my perspectives. These hobbies keep me balanced and energized
